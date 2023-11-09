7755ca2112959952ac19ea0d089f531fce37a30f
William Oliveira in House <will-belo>
2023-11-09 08:04:51 -0300

    criação das migrations

---
+++ database/migrations/2023_11_09_105505_create-skins-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skins', function(Blueprint $table){
+            $table->id();
+            $table->string('weapon');
+            $table->string('skin_name');
+            $table->string('float');
+            $table->double('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
